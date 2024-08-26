9031fa10582f588c64199fbee7d53d8f6d8a3bda
Rodolfo Grijalva <fitogm7@gmail.com>
2024-08-26 09:36:15 -0700

    Se agregó filtrado de datos por tipo de gas.

--- RodoApp.py
+++ RodoApp.py
@@ -6,19 +6,23 @@ car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
 st.header('Análisis de Datos de Vehículos en Venta')
 
+# Filtrado de datos
+fuel_type = st.sidebar.selectbox('Selecciona el tipo de combustible', car_data['fuel'].unique())
+filtered_data = car_data[car_data['fuel'] == fuel_type]
+
 # Casilla de verificación para el histograma
 hist_checkbox = st.sidebar.checkbox('Mostrar histograma')
 
 if hist_checkbox:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(filtered_data, x="odometer", nbins=30, title='Distribución del Kilometraje')
+    fig.update_layout(xaxis_title='Kilometraje', yaxis_title='Frecuencia')
     st.plotly_chart(fig, use_container_width=True)
-
 
 # Casilla de verificación para el gráfico de dispersión
 scatter_checkbox = st.sidebar.checkbox('Mostrar gráfico de dispersión')
 
 if scatter_checkbox:
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
-    scatter_fig = px.scatter(car_data, x="odometer", y="price", title='Gráfico de Dispersión del Odómetro vs Precio', labels={'odometer': 'Kilometraje', 'price': 'Precio'}, color_discrete_sequence=['blue'])
+    scatter_fig = px.scatter(filtered_data, x="odometer", y="price", color="condition", title='Gráfico de Dispersión del Odómetro vs Precio', labels={'odometer': 'Kilometraje', 'price': 'Precio'}, trendline="ols")
     st.plotly_chart(scatter_fig, use_container_width=True)
