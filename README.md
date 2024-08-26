17e132b36f2b2eb51614a781761f477218135025
Rodolfo Grijalva <fitogm7@gmail.com>
2024-08-26 08:49:33 -0700

    Cambio de botones a casillas de verificación

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RodolfoGM
 Rodolfito repository
+
+Ésta aplicación contiene dos casillas de verificación que crean un histograma y un diagrama de dispersión en base al conjunto de datos proporcionado. 
--- app.py
+++ app.py
@@ -3,29 +3,22 @@ import plotly.express as px
 import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
 st.header('Análisis de Datos de Vehículos en Venta')
-hist_button = st.button('Construir histograma') # crear un botón
+
+# Casilla de verificación para el histograma
+hist_checkbox = st.sidebar.checkbox('Mostrar histograma')
+
+if hist_checkbox:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
 
 
-        
-if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-            # crear un histograma
-            fig = px.histogram(car_data, x="odometer")
-        
-            # mostrar un gráfico Plotly interactivo
-            st.plotly_chart(fig, use_container_width=True)
+# Casilla de verificación para el gráfico de dispersión
+scatter_checkbox = st.sidebar.checkbox('Mostrar gráfico de dispersión')
 
-
-# Botón para construir el gráfico de dispersión
-scatter_button = st.button('Construir gráfico de dispersión')
-
-if scatter_button:
+if scatter_checkbox:
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
-    
-    # Crear un gráfico de dispersión con personalización
     scatter_fig = px.scatter(car_data, x="odometer", y="price", title='Gráfico de Dispersión del Odómetro vs Precio', labels={'odometer': 'Kilometraje', 'price': 'Precio'}, color_discrete_sequence=['blue'])
-    
     st.plotly_chart(scatter_fig, use_container_width=True)
